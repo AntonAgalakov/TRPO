fe2a8fb586d8bda28e7a7d8f72d6a6e03ef26454
Агалаков Антон Александрович <is641s01@lwp10.csc.local>
2017-02-20 08:12:40 +0700

    Add data entry

--- lab1.c
+++ lab1.c
@@ -2,5 +2,20 @@
 
 int main()
 {
-
+	int skok = -1;
+	int vklad = 1;
+	
+	while ((srok < 0) && (srok <=365)) {
+		printf("Enter the term of the deposit: ");
+		scanf("%d", &srok);
+		if (srok < 0)
+			printf("\nThe data is not correct");
+	}
+	
+	while (vklad < 10000) {
+		printf("Enter the amount of your deposit: ");
+		scanf("%d", &vklad);
+		if (vklad < 10000)
+			printf("\n The data is not correct");
+	}
 }
